d9c6b6ee1c1dce44fefbe31228dab10e9ca29907
IvardenBraber <trolled@Live.nl>
2018-01-22 19:10:47 +0100

    ed
    ddd

--- motors.py
+++ motors.py
@@ -1,109 +1,57 @@
-#!/usr/bin/python
-# Import required libraries
-import sys
-import time
 import RPi.GPIO as GPIO
- 
-# Use BCM GPIO references
-# instead of physical pin numbers
+from time import sleep
+
 GPIO.setmode(GPIO.BCM)
- 
-# Define GPIO signals to use
-# Physical pins 11,15,16,18
-# GPIO17,GPIO22,GPIO23,GPIO24
-StepPinsR = [2,3,4,17]
-StepPinsL = [27,22,10,9]
 
-# Define speed of the wheels
-Speed	= 0.0005
- 
-# Set all pins as output
-for pin in StepPinsL:
-  print "Setup pins"
-  GPIO.setup(pin,GPIO.OUT)
-  GPIO.output(pin, False)
+Motor1A = 2
+Motor1B = 3
+Motor1C = 4
+Motor1D = 17
 
-# Set all pins as output
-for pin in StepPinsR:
-  print "Setup pins"
-  GPIO.setup(pin,GPIO.OUT)
-  GPIO.output(pin, False)
+Motor2A = 27
+Motor2B = 22
+Motor2C = 10
+Motor2D = 9
 
-  
-# Define advanced sequence
-# as shown in manufacturers datasheet
-Seq = [
-   [1,0,0,1],
-   [1,0,0,0],
-   [1,1,0,0],
-   [0,1,0,0],
-   [0,1,1,0],
-   [0,0,1,0],
-   [0,0,1,1],
-   [0,0,0,1]
-]
-        
-StepCount 	= len(Seq)
-StepDirR 	= 1 # Set to 1 or 2 for clockwise
-            # Set to -1 or -2 for anti-clockwise
-StepDirL 	= -1; 
-# Read wait time from command line
-if len(sys.argv)>1:
-  WaitTime = int(sys.argv[1])/float(1000)
-else:
-  WaitTime = 10/float(1000)
- 
-# Initialise variables
-StepCounterL = 0
-StepCounterR = 0
- 
- 
-def turnLeftWheel( StepCounterL ):
-	#LEFT PINS
-	for pin in range(0,4):
-		xpin=StepPinsL[pin]# Get GPIO
-		if Seq[StepCounterL][pin]!=0:
-		  print " Enable GPIO %i" %(xpin)
-		  GPIO.output(xpin, True)
-		else:
-		  GPIO.output(xpin, False)
 
-	StepCounterL += StepDirL
+GPIO.setup(Motor1A,GPIO.OUT)
+GPIO.setup(Motor1B,GPIO.OUT)
+GPIO.setup(Motor1C,GPIO.OUT)
+GPIO.setup(Motor1D,GPIO.OUT)
 
-	# If we reach the end of the sequence
-	# start again
-	if (StepCounterL>=StepCount):
-		StepCounterL = 0
-	if (StepCounterL<0):
-		StepCounterL = StepCount+StepDirL
+GPIO.setup(Motor2A,GPIO.OUT)
+GPIO.setup(Motor2B,GPIO.OUT)
+GPIO.setup(Motor2C,GPIO.OUT)
+GPIO.setup(Motor2D,GPIO.OUT)
 
-	return StepCounterL
-	
-def turnRightWheel( StepCounterR ):
-	for pin in range(0,4):
-		xpin=StepPinsR[pin]# Get GPIO
-		if Seq[StepCounterR][pin]!=0:
-			print " Enable GPIO %i" %(xpin)
-			GPIO.output(xpin, True)
-		else:
-			GPIO.output(xpin, False)
-
-	StepCounterR += StepDirR
-
+print "Going forwards"
+GPIO.output(Motor1A,GPIO.HIGH)
+GPIO.output(Motor1B,GPIO.LOW)
+GPIO.output(Motor1C,GPIO.HIGH)
+GPIO.output(Motor1D,GPIO.LOW)
  
-	# If we reach the end of the sequence
-	# start again
-	if (StepCounterR>=StepCount):
-		StepCounterR = 0
-	if (StepCounterR<0):
-		StepCounterR = StepCount+StepDirR
-	
-	return StepCounterR
+GPIO.output(Motor2A,GPIO.HIGH)
+GPIO.output(Motor2B,GPIO.LOW)
+GPIO.output(Motor2C,GPIO.HIGH)
+GPIO.output(Motor2D,GPIO.LOW)
+
+sleep(2)
  
-# Start main loop
-while True:
-  StepCounterL = turnLeftWheel( StepCounterL )
-  StepCounterR = turnRightWheel( StepCounterR )
+print "Going backwards"
+GPIO.output(Motor1A,GPIO.LOW)
+GPIO.output(Motor1B,GPIO.HIGH)
+GPIO.output(Motor1C,GPIO.HIGH)
+GPIO.output(Motor1D,GPIO.HIGH)
  
-  # Wait before moving on
-  time.sleep(Speed)
+GPIO.output(Motor2A,GPIO.LOW)
+GPIO.output(Motor2B,GPIO.HIGH)
+GPIO.output(Motor2C,GPIO.HIGH)
+GPIO.output(Motor2D,GPIO.HIGH)
+ 
+sleep(2)
+
+print "Now stop"
+GPIO.output(Motor1D,GPIO.LOW)
+GPIO.output(Motor2D,GPIO.LOW)
+ 
+GPIO.cleanup()
