eb2b9774e052a23c60b664293de6cb49047fd151
raphaelmeijer <info@raphaelmeijer.nl>
2018-01-22 16:56:13 +0100

    first test code

--- motors.py
+++ motors.py
@@ -1,57 +1,70 @@
+#!/usr/bin/python
+# Import required libraries
+import sys
+import time
 import RPi.GPIO as GPIO
-from time import sleep
-
+ 
+# Use BCM GPIO references
+# instead of physical pin numbers
 GPIO.setmode(GPIO.BCM)
-
-Motor1A = 2
-Motor1B = 3
-Motor1C = 4
-Motor1D = 17
-
-Motor2A = 27
-Motor2B = 22
-Motor2C = 10
-Motor2D = 9
-
-
-GPIO.setup(Motor1A,GPIO.OUT)
-GPIO.setup(Motor1B,GPIO.OUT)
-GPIO.setup(Motor1C,GPIO.OUT)
-GPIO.setup(Motor1D,GPIO.OUT)
-
-GPIO.setup(Motor2A,GPIO.OUT)
-GPIO.setup(Motor2B,GPIO.OUT)
-GPIO.setup(Motor2C,GPIO.OUT)
-GPIO.setup(Motor2D,GPIO.OUT)
-
-print "Going forwards"
-GPIO.output(Motor1A,GPIO.HIGH)
-GPIO.output(Motor1B,GPIO.LOW)
-GPIO.output(Motor1C,GPIO.LOW)
-GPIO.output(Motor1D,GPIO.HIGH)
  
-GPIO.output(Motor2A,GPIO.HIGH)
-GPIO.output(Motor2B,GPIO.LOW)
-GPIO.output(Motor2C,GPIO.LOW)
-GPIO.output(Motor2D,GPIO.HIGH)
-
-sleep(2)
+# Define GPIO signals to use
+# Physical pins 11,15,16,18
+# GPIO17,GPIO22,GPIO23,GPIO24
+StepPins = [2,3,4,17]
  
-print "Going backwards"
-GPIO.output(Motor1A,GPIO.LOW)
-GPIO.output(Motor1B,GPIO.HIGH)
-GPIO.output(Motor1C,GPIO.HIGH)
-GPIO.output(Motor1D,GPIO.HIGH)
+# Set all pins as output
+for pin in StepPins:
+  print "Setup pins"
+  GPIO.setup(pin,GPIO.OUT)
+  GPIO.output(pin, False)
  
-GPIO.output(Motor2A,GPIO.LOW)
-GPIO.output(Motor2B,GPIO.HIGH)
-GPIO.output(Motor2C,GPIO.HIGH)
-GPIO.output(Motor2D,GPIO.HIGH)
+# Define advanced sequence
+# as shown in manufacturers datasheet
+Seq = [[1,0,0,1],
+       [1,0,0,0],
+       [1,1,0,0],
+       [0,1,0,0],
+       [0,1,1,0],
+       [0,0,1,0],
+       [0,0,1,1],
+       [0,0,0,1]]
+        
+StepCount = len(Seq)
+StepDir = 1 # Set to 1 or 2 for clockwise
+            # Set to -1 or -2 for anti-clockwise
  
-sleep(2)
-
-print "Now stop"
-GPIO.output(Motor1E,GPIO.LOW)
-GPIO.output(Motor2E,GPIO.LOW)
+# Read wait time from command line
+if len(sys.argv)>1:
+  WaitTime = int(sys.argv[1])/float(1000)
+else:
+  WaitTime = 10/float(1000)
  
-GPIO.cleanup()
+# Initialise variables
+StepCounter = 0
+ 
+# Start main loop
+while True:
+ 
+  print StepCounter,
+  print Seq[StepCounter]
+ 
+  for pin in range(0,4):
+    xpin=StepPins[pin]# Get GPIO
+    if Seq[StepCounter][pin]!=0:
+      print " Enable GPIO %i" %(xpin)
+      GPIO.output(xpin, True)
+    else:
+      GPIO.output(xpin, False)
+ 
+  StepCounter += StepDir
+ 
+  # If we reach the end of the sequence
+  # start again
+  if (StepCounter>=StepCount):
+    StepCounter = 0
+  if (StepCounter<0):
+    StepCounter = StepCount+StepDir
+ 
+  # Wait before moving on
+  time.sleep(WaitTime)
