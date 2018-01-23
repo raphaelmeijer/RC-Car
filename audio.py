e829b0e72029684671386002d422fe8272c928a4
IvardenBraber <trolled@Live.nl>
2018-01-23 09:28:39 +0100

    button event
    speelt geluid af wanneer de knop is ingedrukt

--- audio.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import os, random
-
-def rndmp3 ():
-	randomfile = random.choice(os.listdir("/home/pi/music/"))
-	file = ' /home/pi/music/'+ randomfile
-	os.system ('mplayer' + file)
-
-rndmp3 ()
